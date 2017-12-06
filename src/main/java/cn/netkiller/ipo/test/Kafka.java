0b3b2145ff04db52a6500e6ae6acad27abb5e0c6
Neo Chan <netkiller@msn.com>
2017-12-06 14:39:21 +0800

    kafka

---
+++ src/main/java/cn/netkiller/ipo/test/Kafka.java
@@ -0,0 +1,34 @@
+package cn.netkiller.ipo.test;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class Kafka {
+
+	private static KafkaConsumer<String, String> consumer;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "123.207.61.225:9092");
+		props.put("group.id", "test");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "1000");
+		props.put("session.timeout.ms", "30000");
+		props.put("log.level", "info");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		consumer = new KafkaConsumer<String, String>(props);
+		consumer.subscribe(Arrays.asList("test"));
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(100);
+			for (ConsumerRecord<String, String> record : records)
+				System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+		}
+	}
+
+}
