9c2f263a16c0dc8cceacaba899f20227c81cc704
Neo Chan <netkiller@msn.com>
2023-04-28 11:03:24 +0800

    编译通过

---
+++ src/main/java/cn/netkiller/thread/NetkillerThreadManager.java
@@ -0,0 +1,141 @@
+package cn.netkiller.thread;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/*
+ * @author neo<netkiller@msn.com>
+ */
+@Slf4j
+@Service
+public class NetkillerThreadManager {
+
+    public Thread[] listThreads() {
+        int count = Thread.activeCount();
+        Thread[] threads = new Thread[count];
+        Thread.enumerate(threads);
+        return threads;
+    }
+
+    public Thread fetchThread(long threadId) {
+        int count = Thread.activeCount();
+        Thread[] threads = new Thread[count];
+        Thread.enumerate(threads);
+        for (Thread thread : threads) {
+            if (thread.threadId() == threadId) {
+                return thread;
+            }
+        }
+        return null;
+    }
+
+    public Thread fetchThread(String name) {
+        return this.fetchThread(name, false);
+    }
+
+    public Thread fetchThread(String name, boolean ignoreCase) {
+        Thread[] threadArray = listThreads();
+        for (Thread thread : threadArray) {
+            if (ignoreCase) {
+                if (thread.getName().equalsIgnoreCase(name)) {
+                    return thread;
+                }
+            } else {
+                if (thread.getName().equals(name)) {
+                    return thread;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public int activeCount() {
+        return Thread.activeCount();
+    }
+
+
+    public void stop(Long threadId) {
+        Thread thread = this.fetchThread(threadId);
+        System.out.println(thread.toString());
+//        thread.interrupt();
+        thread.stop();
+    }
+
+    public void stop(String name) {
+        Thread thread = this.fetchThread(name);
+        thread.stop();
+    }
+
+    public void interrupt(Long threadId) {
+        Thread thread = this.fetchThread(threadId);
+        System.out.println(thread.toString());
+        thread.interrupt();
+    }
+
+    public void interrupt(String name) {
+        Thread thread = this.fetchThread(name);
+        thread.interrupt();
+    }
+
+    public void wait(Long threadId) {
+        Thread thread = this.fetchThread(threadId);
+        try {
+            thread.wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void wait(String name) {
+        Thread thread = this.fetchThread(name);
+        try {
+            thread.wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void start(Long threadId) {
+        Thread thread = this.fetchThread(threadId);
+        thread.start();
+    }
+
+    public void start(String name) {
+        Thread thread = this.fetchThread(name);
+        thread.start();
+    }
+
+    public String show() {
+        StringBuilder out = new StringBuilder();
+        Thread[] threads = this.listThreads();
+        int idLength = 4;
+        int nameLength = 4;
+        int groupLength = 5;
+        for (Thread thread : threads) {
+
+            if (Long.toString(thread.threadId()).length() > idLength) {
+                idLength = Long.toString(thread.threadId()).length();
+            }
+            if (thread.getName().length() > nameLength) {
+                nameLength = thread.getName().length();
+            }
+            if (thread.getThreadGroup().getName().length() > groupLength) {
+                groupLength = thread.getThreadGroup().getName().length();
+            }
+        }
+
+        String format = "| %" + idLength + "s | %" + nameLength + "s | %" + groupLength + "s | %6s | %13s | %8s |\n";
+        String line = String.format("%0" + (idLength + nameLength + groupLength + 30 + 16) + "d\n", 0).replace("0", "=");
+        out.append(line);
+        out.append(String.format(format, "ID", "Name", "Group", "Daemon", "State", "Priority"));
+        out.append(line.replace("=", "-"));
+        for (Thread thread : threads) {
+            if (thread != null) {
+                out.append(String.format(format, thread.threadId(), thread.getName(), thread.getThreadGroup().getName(), thread.isDaemon(), thread.getState().name(), thread.getPriority()));
+            }
+        }
+        out.append(line);
+        return out.toString();
+    }
+}
---
+++ src/main/java/cn/netkiller/thread/TestThread.java
@@ -0,0 +1,176 @@
+package cn.netkiller.thread;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.*;
+
+import static java.util.stream.IntStream.*;
+
+
+public class TestThread {
+
+//    private static final Logger logger = LoggerFactory.getLogger(TestThread.class);
+
+    public TestThread() {
+
+    }
+
+    public static void setThreadName1() {
+        new Thread("thread-name-1") {
+            public void run() {
+                try {
+                    Thread.sleep(1000 * 15);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("threadName1: " + this.getName());
+
+            }
+        }.start();
+    }
+
+    public static void setThreadName2() {
+        new Thread() {
+            @SneakyThrows
+            public void run() {
+                this.setName("thread-name-2");
+                Thread.sleep(1000 * 15);
+                System.out.println("threadName2: " + this.getName());
+
+            }
+        }.start();
+    }
+
+    public static void setThreadName3() {
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(1000 * 15);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("threadName3: " + this.getName());
+
+            }
+        };
+
+        thread.setName("thread-name-3");
+        thread.start();
+    }
+
+    public static void setThreadName4() {
+        new Thread("测试线程-1") {
+            public void run() {
+                try {
+                    Thread.sleep(1000 * 15);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("threadName1: " + this.getName());
+
+            }
+        }.start();
+    }
+
+    public static void threadLocal() {
+        ThreadLocal<String> local = new ThreadLocal<>();
+
+        IntStream.range(0, 10).forEach(i -> new Thread(() -> {
+            local.set(Thread.currentThread().getName() + ":" + i);
+            System.out.println("线程：" + Thread.currentThread().getName() + ",local:" + local.get());
+        }).start());
+    }
+
+    public static void inheritableThreadLocal() {
+        InheritableThreadLocal threadLocal = new InheritableThreadLocal();
+        IntStream.range(0, 10).forEach(i -> {
+            //每个线程的序列号，希望在子线程中能够拿到
+            threadLocal.set(i);
+            //这里来了一个子线程，我们希望可以访问上面的threadLocal
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get());
+            }).start();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        NetkillerThreadManager ntm = new NetkillerThreadManager();
+//        System.out.println(Arrays.stream(ntm.listThreads()).toList());
+
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            threadPool.execute(() -> {
+                try {
+                    Thread.sleep(1000 * 30);
+                } catch (InterruptedException e) {
+                    System.out.println("中断Thread#==" + finalI);
+//                        e.printStackTrace();
+                }
+
+            });
+        }
+        threadPool.shutdown();
+
+        TestThread.threadLocal();
+        TestThread.inheritableThreadLocal();
+//        System.out.println(ntm.activeCount());
+        TestThread.setThreadName1();
+        TestThread.setThreadName2();
+        TestThread.setThreadName3();
+        TestThread.setThreadName4();
+
+        System.out.println(ntm.show());
+        ntm.stop("pool-1-thread-1");
+        ntm.stop("pool-1-thread-2");
+        ntm.interrupt("pool-1-thread-3");
+        //ThreadExplorer.kill("pool-1-thread-*", truer, "");//stop所有线程名符合pool-1-thread-*的线程
+//        System.out.println(ntm.fetchThread("pool-1-thread-8").toString());
+//        System.out.println(ntm.fetchThread(25).toString());
+        ntm.stop(28L);
+        ntm.interrupt(30L);
+//        ntm.wait(30L);
+        Thread.sleep(1000);// 休眠只是为了后面能够打印的时候准确显示
+
+        System.out.println(ntm.show());
+
+        Thread.sleep(30000);// 休眠只是为了后面能够打印的时候准确显示
+        System.out.println(ntm.show());
+
+        // final UUID uuid = UUID();
+        // static final SecureRandom secureRandom = new SecureRandom();
+        // secureRandom.
+
+        // System.out.println(uuid.toString());
+
+//		System.out.println(id);
+        // Marker finance = MarkerFactory.getMarker(LogMarker.finance.toString());
+        // Marker customer = MarkerFactory.getMarker(LogMarker.customer.toString());
+        // Marker market = MarkerFactory.getMarker(LogMarker.market.toString());
+        // logger.info("AAAAAAAAA");
+        // logger.info(finance, "BBBBBBBBB");
+        // logger.error(finance, "This is a serious an error requiring the admin's attention", new Exception("Just testing"));
+        // logger.info(finance, "BBBBBBBBB");
+        // logger.info(customer, "BBBBBBBBB");
+        // logger.info(market, "BBBBBBBBB");
+
+        // MDC.put("userId","0001");
+        // logger.info("0001用户");
+        // MDC.clear(); 
+
+        // MDC.put("userId","0002");
+        // logger.info("0002用户");
+        // MDC.clear();
+
+    }
+
+}
